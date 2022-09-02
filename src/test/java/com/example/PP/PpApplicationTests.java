21033a70a97d4327acb47f43b3de5983d5d2b2ed
LMathers00 <lukemathers@hotmail.co.uk>
2022-09-02 14:39:20 +0100

    Initial springboot set-up

---
+++ src/test/java/com/example/PP/PpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
